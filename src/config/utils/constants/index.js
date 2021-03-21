2ca9f7d7d14a51c8e5e95e887fca2b4b5ac57cdc
filzaharry <62638551+filzaharry@users.noreply.github.com>
2021-03-21 13:36:50 +0700

    brave

--- src/config/utils/constants/index.js
+++ src/config/utils/constants/index.js
@@ -1,5 +1,5 @@
-// export const API = 'http://localhost:4000/';
-export const API = 'https://aplus-hrd-api-server.herokuapp.com/';
+export const API = 'http://localhost:4000/';
+// export const API = 'https://aplus-hrd-api-server.herokuapp.com/';
 // export const API_URL = 'http://localhost:4000/v1/hrd/';
 export const API_URL = 'https://aplus-hrd-api-server.herokuapp.com/v1/hrd/';
 // export const API_URL_SPV = 'http://localhost:4000/v1/spv/';
