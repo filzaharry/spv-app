f6e21c2dba85692d0bdc93c8d47a12ff22a17555
filzaharry <62638551+filzaharry@users.noreply.github.com>
2021-03-22 19:13:25 +0700

    hehe

--- src/config/utils/constants/index.js
+++ src/config/utils/constants/index.js
@@ -1,6 +1,6 @@
-export const API = 'http://localhost:4000/';
-// export const API = 'https://aplus-hrd-api-server.herokuapp.com/';
-export const API_URL = 'http://localhost:4000/v1/hrd/';
-// export const API_URL = 'https://aplus-hrd-api-server.herokuapp.com/v1/hrd/';
-export const API_URL_SPV = 'http://localhost:4000/v1/spv/';
-// export const API_URL_SPV = 'https://aplus-hrd-api-server.herokuapp.com/v1/spv/';
+// export const API = 'http://localhost:4000/';
+export const API = 'https://aplus-hrd-api-server.herokuapp.com/';
+// export const API_URL = 'http://localhost:4000/v1/hrd/';
+export const API_URL = 'https://aplus-hrd-api-server.herokuapp.com/v1/hrd/';
+// export const API_URL_SPV = 'http://localhost:4000/v1/spv/';
+export const API_URL_SPV = 'https://aplus-hrd-api-server.herokuapp.com/v1/spv/';
